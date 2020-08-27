74c3f5d8357dc4646c2dad08995c334f1ca706cd
Scream <skate_max@mail.ru>
2020-08-27 14:21:35 +0300

    Updates

---
+++ precache-manifest.99da199804375d661eaca6de40fd127d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a39a00215bfec12bd18b77c33b3dae20",
+    "url": "/test-react-site1/index.html"
+  },
+  {
+    "revision": "c094246de41a88705e02",
+    "url": "/test-react-site1/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "5950c2d65fc7d532ccb7",
+    "url": "/test-react-site1/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c094246de41a88705e02",
+    "url": "/test-react-site1/static/js/2.29c7e8bc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/test-react-site1/static/js/2.29c7e8bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5950c2d65fc7d532ccb7",
+    "url": "/test-react-site1/static/js/main.8159362c.chunk.js"
+  },
+  {
+    "revision": "ce2de439551e05cdd00a",
+    "url": "/test-react-site1/static/js/runtime-main.c1a17c95.js"
+  },
+  {
+    "revision": "580ffd26ccaa16481ed87c1829c74943",
+    "url": "/test-react-site1/static/media/1200px-React-icon.svg.580ffd26.png"
+  },
+  {
+    "revision": "5684fb958ed7d8502118acbba9825b75",
+    "url": "/test-react-site1/static/media/people-coffee-notes-tea.5684fb95.jpg"
+  },
+  {
+    "revision": "f2051222d65b3eed17da17bae9e7b67d",
+    "url": "/test-react-site1/static/media/people-woman-coffee-meeting.f2051222.jpg"
+  },
+  {
+    "revision": "b9a1fbe55c091c001fe5fedc631cb0f4",
+    "url": "/test-react-site1/static/media/pexels-photo-1000445.b9a1fbe5.jpeg"
+  },
+  {
+    "revision": "550f6931d4a3d911856d143e00dcd6e0",
+    "url": "/test-react-site1/static/media/pexels-photo-1181622.550f6931.jpeg"
+  },
+  {
+    "revision": "6b8db2bbc7043d4818c8dbcb51e4f33b",
+    "url": "/test-react-site1/static/media/pexels-photo-3183150.6b8db2bb.jpeg"
+  },
+  {
+    "revision": "e6ed7524ce0ca18a97ea971492fc044e",
+    "url": "/test-react-site1/static/media/pexels-photo-3183153.e6ed7524.jpeg"
+  },
+  {
+    "revision": "cee2600901f8f0516b6a95fe77fd794e",
+    "url": "/test-react-site1/static/media/pexels-photo-3184160.cee26009.jpeg"
+  },
+  {
+    "revision": "ae8361bf822716ef58a2593215094886",
+    "url": "/test-react-site1/static/media/pexels-photo-3184418.ae8361bf.jpeg"
+  },
+  {
+    "revision": "4e6467b99df3828398e6573376d312e7",
+    "url": "/test-react-site1/static/media/play-stone-network-networked-interactive-163064.4e6467b9.jpeg"
+  }
+]);
